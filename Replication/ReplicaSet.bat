d1bc7c615253a3b1e8e6011cb8329d4259116aa0
Mahmoud Khattab <m_khatab_88@hotmail.com>
2017-08-25 22:48:05 +0200

    Fix replSet switch (in replication command)

--- Replication/ReplicaSet.bat
+++ Replication/ReplicaSet.bat
@@ -5,10 +5,10 @@ md \db2
 md \db3
 
 @REM Primary
-start "a" mongod --dbpath ./db1 --port 30000 --replset "demo"
+start "a" mongod --dbpath ./db1 --port 30000 --replSet "demo"
 
 @REM Secondary
-start "b" mongod --dbpath ./db2 --port 40000 --replset "demo"
+start "b" mongod --dbpath ./db2 --port 40000 --replSet "demo"
 
 @REM Arbitrary
-start "c" mongod --dbpath ./db3 --port 50000 --replset "demo"
+start "c" mongod --dbpath ./db3 --port 50000 --replSet "demo"
