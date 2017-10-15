f7523464e55d9600d45851c74e6193dceaecf618
Mahmoud Khattab <m_khatab_88@hotmail.com>
2017-10-15 19:05:07 +0400

    update client app

--- MongoDB-POS/Client/Program.cs
+++ MongoDB-POS/Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Driver;
+using Providers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,16 +15,31 @@ namespace Client
         {
             Console.WriteLine("Hello MongoDB !");
 
-            var client = new MongoClient("mongodb://localhost:27017");
-            var db = client.GetDatabase("test");
-            var collection = db.GetCollection<BsonDocument>("users");
+            var connStr = "mongodb://localhost:27017";
+            var mongo = new MongoClient();
+            IDbProvider provider = new MongoDbProvider(connStr, mongo);
+
+            // get client
+            var client = provider.CreateClient(connStr);
+            Console.WriteLine(client);
+
+            // get database
+            var db = provider.GetDatabase("users");
+            Console.WriteLine(db);
+
+
+
+
+            //var client = new MongoClient("mongodb://localhost:27017");
+            //var db = client.GetDatabase("test");
+            //var collection = db.GetCollection<BsonDocument>("users");
 
             //var document = new BsonDocument();
             //document.Add(new BsonElement("name", "ahmed"));
             //collection.InsertOne(document);
 
-            var document = collection.Find(new BsonDocument()).FirstOrDefault();
-            Console.WriteLine(document);
+            //var document = collection.Find(new BsonDocument()).FirstOrDefault();
+            //Console.WriteLine(document);
 
             //var count = collection.Count(new BsonDocument());
             //Console.WriteLine(count);
