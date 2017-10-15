b95c061ee338b181eca114193d1f6ea43eaf487c
Mahmoud Khattab <m_khatab_88@hotmail.com>
2017-10-15 16:14:52 +0400

    connect to mongo using driver

--- MongoDB-POS/Client/Program.cs
+++ MongoDB-POS/Client/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +13,13 @@ namespace Client
         static void Main(string[] args)
         {
             Console.WriteLine("Hello MongoDB !");
+
+            var client = new MongoClient("mongodb://localhost:27017");
+            var db = client.GetDatabase("test");
+            var users = db.GetCollection<BsonDocument>("users");
+
+            var usersCount = users.Count(new BsonDocument());
+            Console.WriteLine(usersCount);
         }
     }
 }
